e5edb99dbde58b4f1eecec656f88a69c3ff115f0
jokersunited <jshwee96@gmail.com>
2021-05-22 03:02:55 +0800

    updated node packages

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path')
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const nunjucks  = require('nunjucks');
 
 const room = require('./room')
 
